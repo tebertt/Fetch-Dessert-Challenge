6180f61b4dbf7c83b751ec162ebc21af2b0a1045
Troy Ebert <troy.ebert@outlook.com>
2023-11-09 09:31:19 -0800

    Initial Commit

---
+++ Dessert App Challenge/Dessert_App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_App_ChallengeApp.swift
+//  Dessert App Challenge
+//
+//  Created by Troy Ebert on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dessert App ChallengeUITests/Dessert_App_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dessert_App_ChallengeUITestsLaunchTests.swift
+//  Dessert App ChallengeUITests
+//
+//  Created by Troy Ebert on 11/9/23.
+//
+
+import XCTest
+
+final class Dessert_App_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
